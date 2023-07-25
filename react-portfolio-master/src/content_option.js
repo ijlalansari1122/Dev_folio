8f69d258e36fba860f6d7be8b4af1ef6191f3c76
ijlalansari1122 <ansariijlal90@gmail.com>
2023-07-25 18:23:48 +0500

    files changed

---
+++ react-portfolio-master/src/content_option.js
@@ -0,0 +1,143 @@
+const logotext = "Ansari";
+const meta = {
+    title: "Ijlal ansari",
+    description: "I’m Ijlalansari Frontend Developer Python devloper,currently Learning Blockchain",
+};
+
+const introdata = {
+    title: "I’m  Ijlal ansari",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Web-apps",
+    },
+    description: "your vision will be changed into reality",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: " about my self",
+    aboutme: ".",
+};
+const worktimeline = [{
+        jobtitle: "Web Admin",
+        where: "GB-Wheels",
+        date: "2021-2023",
+    },
+    {
+        jobtitle: "Frontend Developer",
+        where: "Grey Matter Ventures",
+        date: "2022-current",
+    },
+    {
+        jobtitle: "WordPress Developer",
+        where: "Northern Hope",
+        date: "2022-current",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Version Control System",
+        value: 85,
+    },  {
+        name: "Solidity",
+        value: 85,
+    }
+];
+
+const services = [{
+        title: "Web Apps",
+        description: "Managing react Web-apps",
+    },
+    {
+        title: "Website Development",
+        description: "Frontend Website development ",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Managing WordPress  Websites .",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "GB-Wheels is a platform for buying and selling cars.",
+        link: "https://gbwheels.com",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Website develop for sid-assist.",
+        link: "https://sidassist.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Website for Grey Matter Ventures.",
+        link: "https://greymatter.com.pk/",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    }  
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ansariijlal90@gmail.com",
+    YOUR_FONE: "+923554300327",
+    description: "Feel free to contact ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ijlalansari1122",
+    linkedin: "https://linkedin.com/in/ijlal-ansari-56b0371b0",
+    twitter: "https://twitter.com/IjlalAnsari1",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
